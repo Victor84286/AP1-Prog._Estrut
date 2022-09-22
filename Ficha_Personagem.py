424f135c20e4f3236cb8c69c746d0d472d019d92
MatheuzH <110929706+MatheuzH@users.noreply.github.com>
2022-09-22 07:25:21 -0300

    Create Ficha_Personagem.py

---
+++ Ficha_Personagem.py
@@ -0,0 +1,17 @@
+"""
+Monta uma ficha de personagem de Dangeons and Dragons
+"""
+def salva_ficha():
+
+def coleta_info():
+
+def rola_dados():
+
+def apresenta_valores():
+
+def escolha_de_atributos():
+
+def corrige_para_raca():
+  
+  
+ 
